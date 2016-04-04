08e99d19d1a6d5ff4933c9672d1da69fe9821734
VICTOR VALENCIA <victor.valencia@gmail.com>
2016-04-03 21:54:16 -0500

    encript

--- www/js/index.js
+++ www/js/index.js
@@ -1,8 +1,10 @@
 function scanear(){
-  alert(22);
    cordova.plugins.barcodeScanner.scan(
       function (result) {
           alert(result.text);
+          var secret = "vinfo";
+          var cryptedMsg = CryptoJS.AES.decrypt(plaintext, secret).toString(CryptoJS.enc.Utf8);
+          alert(cryptedMsg);
       }, 
       function (error) {
           $(".alert-danger").show();
